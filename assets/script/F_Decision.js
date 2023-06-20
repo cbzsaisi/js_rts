0743ca84aea1c4290ad65bef10a68b9692d329ba
cbzsaisi <cbzsaisi@sina.com>
2023-06-20 17:44:17 +0800

    062017

--- assets/script/F_Decision.js
+++ assets/script/F_Decision.js
@@ -4,15 +4,17 @@ var g_GameArray = require("./A_GameArray");
 function F_Decision() {};
 
 F_Decision.RoleAi = function(v_RoleNum,v_D_Time) {
+
+    var Demand = []
     var s_Role = G_P.g_GameDataResManger.GetRole(v_RoleNum);
 
-    this.RoleAiWorkDemand(s_Role);
+    this.RoleAiWorkDemand(s_Role,Demand);
     //console.log(s_Role.Decision.v_Health,s_Role.Decision.v_WorkDemand,v_D_Time);
    
     return null;
 }
 
-F_Decision.RoleAiWorkDemand = function(v_Role) {
+F_Decision.RoleAiWorkDemand = function(v_Role, v_Demand) {
     for(let i = 0; i < v_Role.Decision.v_WorkDemand.length; i++){
         switch(v_Role.Decision.v_WorkDemand[i].Type){
             case G_P.e_WorkDemand.Work_Felling:{
@@ -24,7 +26,7 @@ F_Decision.RoleAiWorkDemand = function(v_Role) {
     return null;
 }
 
-F_Decision.RoleAiFightDemand = function(v_Role) {
+F_Decision.RoleAiFightDemand = function(v_Role, v_Demand) {
     for(let i = 0; i < v_Role.Decision.v_FightDemand.length; i++){
         switch(v_Role.Decision.v_FightDemand[i].Type){
             case G_P.e_FightDemand.AttackRole:{
