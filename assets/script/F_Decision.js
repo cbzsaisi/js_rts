6281acaee6179e4c67f454fcb2f520b49705d67c
cbzsaisi <cbzsaisi@sina.com>
2023-06-25 19:41:15 +0800

    062519

--- assets/script/F_Decision.js
+++ assets/script/F_Decision.js
@@ -8,20 +8,33 @@ F_Decision.RoleAi = function(v_RoleNum,v_D_Time) {
     var Demand = []
     var s_Role = G_P.g_GameDataResManger.GetRole(v_RoleNum);
 
-    this.RoleAiDemand(s_Role,Demand);
-    //console.log(s_Role.Decision.v_Health,s_Role.Decision.v_WorkDemand,v_D_Time);
+    this.RoleAiWorkDemand(s_Role,Demand);
+    //console.log(s_Role.Decision.v_Health,s_Role.Decision.v_Demand,v_D_Time);
    
     return null;
 }
 
-F_Decision.RoleAiDemand = function(v_Role, v_Demand) {
+F_Decision.RoleAiWorkDemand = function(v_Role, v_Demand) {
     for(let i = 0; i < v_Role.Decision.v_Demand.length; i++){
         switch(v_Role.Decision.v_Demand[i].Type){
             case G_P.e_Demand.Work_Felling:{
-                //console.log("e_Demand.Work_Felling")
-                break;
-            }
-            case G_P.e_Demand.AttackRole:{
+                v_Role.Decision.v_Demand[i].Value += v_Role.Decision.v_Demand[i].StepValue;
+                if(v_Role.Decision.v_Demand[i].Value > 9999){
+                    v_Role.Decision.v_Demand[i].Value = 0;
+                    if(v_Role.Decision.v_Demand[i].Level < 10){
+                        v_Role.Decision.v_Demand[i].Level += 1;
+                    }else{
+                        v_Role.Decision.v_Demand[i].StepValue = 0;
+                    }
+                }else if(v_Role.Decision.v_Demand[i].Value < 0){
+                    v_Role.Decision.v_Demand[i].Value = 0;
+                    if(v_Role.Decision.v_Demand[i].Level > 1){
+                        v_Role.Decision.v_Demand[i].Level -= 1;
+                    }else{
+                        v_Role.Decision.v_Demand[i].StepValue = 0;
+                    }
+                };
+                console.log("Value:",v_Role.Decision.v_Demand[i].Value,"level:",v_Role.Decision.v_Demand[i].Level)
                 break;
             }
         }
@@ -29,15 +42,15 @@ F_Decision.RoleAiDemand = function(v_Role, v_Demand) {
     return null;
 }
 
-// F_Decision.RoleAiFightDemand = function(v_Role, v_Demand) {
-//     for(let i = 0; i < v_Role.Decision.v_FightDemand.length; i++){
-//         switch(v_Role.Decision.v_FightDemand[i].Type){
-//             case G_P.e_FightDemand.AttackRole:{
-//                 //console.log("e_WorkDemand.Work_Felling")
-//                 break;
-//             }
-//         }
-//     }
-//     return null;
-// }
+F_Decision.RoleAiFightDemand = function(v_Role, v_Demand) {
+    for(let i = 0; i < v_Role.Decision.v_FightDemand.length; i++){
+        switch(v_Role.Decision.v_FightDemand[i].Type){
+            case G_P.e_Demand.AttackRole:{
+                //console.log("e_WorkDemand.Work_Felling")
+                break;
+            }
+        }
+    }
+    return null;
+}
 module.exports = F_Decision;
